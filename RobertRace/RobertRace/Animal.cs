3878f39ef3388cfe06427955fedb2a8c59f8b6cb
robasik11 <robasik11@gmail.com>
2020-04-14 12:52:17 +0200

    KLasser

---
+++ RobertRace/RobertRace/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobertRace
+{
+    class Animal
+    {
+        public int speed;
+        public int position;
+        public int energycurrent;
+        public int energtMax;
+
+        static Random generator = new Random();
+
+    }
+}
