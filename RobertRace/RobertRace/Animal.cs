3f289a882dfde04ab2e9fe0754ba2cd0d6d7438d
robasik11 <robasik11@gmail.com>
2020-04-15 19:09:11 +0200

    Klasser

--- RobertRace/RobertRace/Animal.cs
+++ RobertRace/RobertRace/Animal.cs
@@ -9,11 +9,39 @@ namespace RobertRace
     class Animal
     {
         public int speed;
-        public int position;
+        public int position = 0;
         public int energycurrent;
-        public int energtMax;
+        public int energyMax = 20;
 
         static Random generator = new Random();
+
+        public void Move()
+        {
+            if (energycurrent >= 1)
+            {
+                position++;
+                energycurrent--;
+                if (energycurrent <= 0)
+                {
+                    energycurrent = 20;
+
+                }
+
+            }
+
+        }
+
+        public void Rest()
+        {
+            energycurrent = energyMax;
+
+        }
+
+        public void Printstats()
+        {
+            
+
+        }
 
     }
 }
